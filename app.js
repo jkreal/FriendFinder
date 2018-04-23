a514e5291337d24d914a93af5f8971c9fc0e5122
jkrowden <jkrowden@gmail.com>
2018-04-23 17:38:12 -0600

    So far...

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var express = require("express");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 
 var app = express();
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "client"));
+app.use(express.static(__dirname + "/client"));
+app.use(express.static(__dirname + "/public"));
 
 
 app.listen(PORT, function() {
